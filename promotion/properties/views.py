8a7d0864b3a7814f15fc63202b5cc3df73307c00
EDZ <mingguangzhen@qfpay.com>
2018-03-13 16:10:59 +0800

    添加get资产图片和拼接过滤json数据的sql方法

--- promotion/properties/views.py
+++ promotion/properties/views.py
@@ -20,7 +20,6 @@ class PropertyViewSet(viewsets.ModelViewSet):
     serializer_class = PropertySerializer
 
     def list(self, request, *args, **kwargs):
-        # import pdb; pdb.set_trace()
         keys = [{'key': 'parms', 'val': (u'性质', u'住宅')},
                 {'key': 'parms', 'val': (u'面积（平米）', (90, 120))},
                 {'key': 'instruction', 'val': (u'债权本金（万）', (0, 2000))},
@@ -40,32 +39,3 @@ class PropertyViewSet(viewsets.ModelViewSet):
             return self.get_paginated_response(serializer.data)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
-
-
-
-#select * from properties_assets where instruction->"$.产权"="90大产权"
-#select * from properties_assets where instruction = CAST('{"产权":"90大产权"}' as JSON)\G;
-#select * from properties_ass ets where instruction = CAST('{"产权":"90大产权"}' as JSON) or spot = CAST('{"配套":"医院"}' as json)\G;
-#select * from properties_assets where instruction->>"$.产权" = "90大产权" or spot->>"$.配套"="医院";
-
-
-# 选择房龄区间  
-# 年代选择
-
-# 选择房产面积区间
-# 面积从大到小
-# 面积从小到大
-
-# 债权从高到低
-# 债权从低到高
-
-# 最新发布
-
-# 选择城市
-
-# 房产性质-->住宅、商业
-# 装修类型
-# select * from properties_assets where parms->>"$.性质" = "住宅" or spot->>"$.配套"="地铁、医院@#！";
-# select * from properties_assets where parms = CAST('{"性质":"住宅"}' as JSON) or parms = CAST('{"朝向":"南北"}' as json)\G;
-# select * from properties_assets where parms = CAST('{"姓名":"明广振"}' as JSON) or parms = CAST('{"姓名":"明广振"}' as json)\G;
-# select * from properties_assets where parms = CAST('{"性质":"住宅", "朝向":"南北"}' as JSON)
