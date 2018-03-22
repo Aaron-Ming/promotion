92ac6b2f6818c61ac5edb539c959f4d47eae1c26
JiaRuibin <jiaruibin@qfpay.com>
2018-03-22 11:05:44 +0800

    修改webpack配置

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,12 +1,13 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var merge = require('webpack-merge');
-var webpackBaseConfig = require('./webpack.config.js');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var merge = require('webpack-merge')
+var webpackBaseConfig = require('./webpack.config.js')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 // 清空基本配置的插件列表
-webpackBaseConfig.plugins = [];
+webpackBaseConfig.plugins = []
 
 module.exports = merge(webpackBaseConfig, {
     // output: {
@@ -15,9 +16,11 @@ module.exports = merge(webpackBaseConfig, {
     //     filename: '[name].js'
     // },
     plugins: [
+        new CleanWebpackPlugin(['statics/css/main.*.css']),
+        new CleanWebpackPlugin(['statics/js/main.*.js']),
         new UglifyJSPlugin(),
         new ExtractTextPlugin({
-            filename: '../css/[name].css',
+            filename: 'css/[name].[hash:8].css',
             allChunks: true
         }),
         // 定义当前 node 环境为生产环境
@@ -33,10 +36,16 @@ module.exports = merge(webpackBaseConfig, {
             }
         }),
         // 提取模板，并保存入口 html 文件
-        // new HtmlWebpackPlugin({
-        //     filename: '../../templates/index.html',
-        //     template: './src/index.html',
-        //     inject: false
-        // })
+        new HtmlWebpackPlugin({
+            filename: '../templates/dashboard.html',
+            title: '钱方运维平台',
+            inject: false,
+            chunks: ['main'], // 加载哪些js 等 静态文件
+            template: './src/index.html',
+            minify: {
+              removeComments: true,
+              collapseWhitespace: false
+            }
+          })
     ]
 });
